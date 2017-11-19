9ed44f28afcbf54e2d5049441542d58496a54a54
bh <jben.huang@gmail.com>
2017-11-19 09:50:24 +0800

    remove duplicated code

--- src/com/refactor/homework/AgedBrie.java
+++ src/com/refactor/homework/AgedBrie.java
@@ -1,49 +1,17 @@
 package com.refactor.homework;
 
-public class AgedBrie extends Item {
-    public AgedBrie(String name, int sellIn, int quality) {
+class AgedBrie extends Item {
+    AgedBrie(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
 
     void updateQuality() {
-        String itemName = this.getName();
-        if ((AGED_BRIE.equals(itemName))) {
-            this.setSellIn(this.getSellIn() - 1);
-            if (this.getQuality() < 50) {
-                this.setQuality(this.getQuality() + 1);
-            }
-            if (this.getQuality() < 50 && this.getSellIn() < 0) {
-                this.setQuality(this.getQuality() + 1);
-            }
-        } else if (BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT.equals(itemName)) {
-            this.setSellIn(this.getSellIn() - 1);
-            if (this.getQuality() < 50) {
-                this.setQuality(this.getQuality() + 1);
-
-                if (this.getSellIn() < 10) {
-                    if (this.getQuality() < 50) {
-                        this.setQuality(this.getQuality() + 1);
-                    }
-                }
-
-                if (this.getSellIn() < 5) {
-                    if (this.getQuality() < 50) {
-                        this.setQuality(this.getQuality() + 1);
-                    }
-                }
-            }
-            if (this.getSellIn() < 0) {
-                this.setQuality(0);
-            }
-        } else if (SULFURAS_HAND_OF_RAGNAROS.equals(itemName)) {
-        } else {
-            this.setSellIn(this.getSellIn() - 1);
-            if (this.getQuality() > 0) {
-                this.setQuality(this.getQuality() - 1);
-            }
-            if (this.getQuality() > 0 && this.getSellIn() < 0) {
-                this.setQuality(this.getQuality() - 1);
-            }
+        this.setSellIn(this.getSellIn() - 1);
+        if (this.getQuality() < 50) {
+            this.setQuality(this.getQuality() + 1);
+        }
+        if (this.getQuality() < 50 && this.getSellIn() < 0) {
+            this.setQuality(this.getQuality() + 1);
         }
     }
 }
--- src/com/refactor/homework/Sulfuras.java
+++ src/com/refactor/homework/Sulfuras.java
@@ -1,50 +1,12 @@
 package com.refactor.homework;
 
 
-public class Sulfuras extends Item {
-    public Sulfuras(String name, int sellIn, int quality) {
+class Sulfuras extends Item {
+    Sulfuras(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
 
     void updateQuality() {
-        String itemName = this.getName();
-        if ((AGED_BRIE.equals(itemName))) {
-            this.setSellIn(this.getSellIn() - 1);
-            if (this.getQuality() < 50) {
-                this.setQuality(this.getQuality() + 1);
-            }
-            if (this.getQuality() < 50 && this.getSellIn() < 0) {
-                this.setQuality(this.getQuality() + 1);
-            }
-        } else if (BACKSTAGE_PASSES_TO_A_TAFKAL80_ETC_CONCERT.equals(itemName)) {
-            this.setSellIn(this.getSellIn() - 1);
-            if (this.getQuality() < 50) {
-                this.setQuality(this.getQuality() + 1);
-
-                if (this.getSellIn() < 10) {
-                    if (this.getQuality() < 50) {
-                        this.setQuality(this.getQuality() + 1);
-                    }
-                }
-
-                if (this.getSellIn() < 5) {
-                    if (this.getQuality() < 50) {
-                        this.setQuality(this.getQuality() + 1);
-                    }
-                }
-            }
-            if (this.getSellIn() < 0) {
-                this.setQuality(0);
-            }
-        } else if (SULFURAS_HAND_OF_RAGNAROS.equals(itemName)) {
-        } else {
-            this.setSellIn(this.getSellIn() - 1);
-            if (this.getQuality() > 0) {
-                this.setQuality(this.getQuality() - 1);
-            }
-            if (this.getQuality() > 0 && this.getSellIn() < 0) {
-                this.setQuality(this.getQuality() - 1);
-            }
-        }
+       //do nothing
     }
 }
