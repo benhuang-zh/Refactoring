d96aea7482055058f95ccd77cb026108ebb54da9
bh <jben.huang@gmail.com>
2017-11-19 08:57:33 +0800

    attr change to private

--- src/com/refactor/homework/Item.java
+++ src/com/refactor/homework/Item.java
@@ -1,9 +1,9 @@
 package com.refactor.homework;
 
 public class Item {
-    public String name;
-    public int sellIn;
-    public int quality;
+    private String name;
+    private int sellIn;
+    private int quality;
 
     private Item(String name, int sellIn, int quality) {
         this.setName(name);
