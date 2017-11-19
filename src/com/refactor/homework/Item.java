6d7b7ac511e55585cf321df86ee86a09d14e247f
bh <jben.huang@gmail.com>
2017-11-19 08:54:38 +0800

    format code

--- src/com/refactor/homework/Item.java
+++ src/com/refactor/homework/Item.java
@@ -2,32 +2,32 @@ package com.refactor.homework;
 
 public class Item {
     public String name;
-	public int sellIn; 
-    public int quality; 
-    
+    public int sellIn;
+    public int quality;
+
     private Item(String name, int sellIn, int quality) {
-		this.setName(name);
-		this.setSellIn(sellIn);
-		this.setQuality(quality);
-	}
+        this.setName(name);
+        this.setSellIn(sellIn);
+        this.setQuality(quality);
+    }
 
-	public static Item createItem(String name, int sellIn, int quality) {
-		return new Item(name, sellIn, quality);
-	}
+    public static Item createItem(String name, int sellIn, int quality) {
+        return new Item(name, sellIn, quality);
+    }
 
-	void updateQuality() {
-		String itemName = this.getName();
+    void updateQuality() {
+        String itemName = this.getName();
         if (("Aged Brie".equals(itemName))) {
             this.setSellIn(this.getSellIn() - 1);
-			if (this.getQuality() < 50) {
+            if (this.getQuality() < 50) {
                 this.setQuality(this.getQuality() + 1);
             }
-			if (this.getQuality() < 50 && this.getSellIn() < 0) {
+            if (this.getQuality() < 50 && this.getSellIn() < 0) {
                 this.setQuality(this.getQuality() + 1);
             }
-		} else if ("Backstage passes to a TAFKAL80ETC concert".equals(itemName)) {
+        } else if ("Backstage passes to a TAFKAL80ETC concert".equals(itemName)) {
             this.setSellIn(this.getSellIn() - 1);
-			if (this.getQuality() < 50) {
+            if (this.getQuality() < 50) {
                 this.setQuality(this.getQuality() + 1);
 
                 if (this.getSellIn() < 10) {
@@ -42,10 +42,10 @@ public class Item {
                     }
                 }
             }
-			if (this.getSellIn() < 0) {
+            if (this.getSellIn() < 0) {
                 this.setQuality(0);
             }
-		} else if ("Sulfuras, Hand of Ragnaros".equals(itemName)) {
+        } else if ("Sulfuras, Hand of Ragnaros".equals(itemName)) {
         } else {
             this.setSellIn(this.getSellIn() - 1);
             if (this.getQuality() > 0) {
@@ -57,23 +57,28 @@ public class Item {
         }
     }
 
-	/* Generated getter and setter code */
+    /* Generated getter and setter code */
     public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getSellIn() {
-		return sellIn;
-	}
-	public void setSellIn(int sellIn) {
-		this.sellIn = sellIn;
-	}
-	public int getQuality() {
-		return quality;
-	}
-	public void setQuality(int quality) {
-		this.quality = quality;
-	}
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
 }
